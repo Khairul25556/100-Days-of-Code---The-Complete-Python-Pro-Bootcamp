a881aae04fe50992a0c7a0cb896ab4c0e377b9a5
Khairul25556 <krft44@gmail.com>
2025-08-28 00:34:42 +0600

    Day-1 tasks completed

--- Day 1/task/task.py
+++ Day 1/task/task.py
@@ -1,2 +1,4 @@
-input("What is your name?")
-
+print("Welcome to the Band Name Generator")
+city = input("Where are you grew up?\n")
+pet = input("What is your pet name?\n")
+print("Your band name is " + city + " " +pet)

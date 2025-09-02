0fb12f022b41bff9cd8982927c5f3988d7f286a9
Khairul25556 <krft44@gmail.com>
2025-09-02 09:06:28 +0600

    Day-2 running

--- Day 2/Type Error, Checking and Conversion/task.py
+++ Day 2/Type Error, Checking and Conversion/task.py
@@ -1,1 +1,11 @@
-len(12345)
+print(type("Hello"))
+print(type(12123243))
+print(type(3.14))
+print(type(True))
+
+print(int("123") + int("123"))
+print(str(123) + str(123))
+print(float(123) + float(123))
+
+name = input("Enter your name: ")
+print("Number of letters in your name: " + str(len(name)))
